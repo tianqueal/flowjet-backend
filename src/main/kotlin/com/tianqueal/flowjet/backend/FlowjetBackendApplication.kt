b58dd0888f355e73dfdcb7e9fa88e6e1112b4b2e
Christian Alvarado <tianqueal@gmail.com>
2025-06-19 08:54:51 +0200

    Initialise Kotlin Spring Boot project with Gradle setup, PostgreSQL configuration, and basic application structure

---
+++ src/main/kotlin/com/tianqueal/flowjet/backend/FlowjetBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.tianqueal.flowjet.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlowjetBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<FlowjetBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/tianqueal/flowjet/backend/FlowjetBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tianqueal.flowjet.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FlowjetBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
