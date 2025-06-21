ab93ee688ef2481485cccbfac70351944d13dccc
Christian Alvarado <tianqueal@gmail.com>
2025-06-21 21:22:40 +0200

    feat: add AuthFunctions for retrieving authenticated username or email

---
+++ src/main/kotlin/com/tianqueal/flowjet/backend/utils/functions/AuthFunctions.kt
@@ -0,0 +1,16 @@
+package com.tianqueal.flowjet.backend.utils.functions
+
+import org.springframework.security.authentication.InsufficientAuthenticationException
+import org.springframework.security.core.context.SecurityContextHolder
+
+object AuthFunctions {
+  fun getAuthenticatedUsernameOrEmail(): String {
+    val auth = SecurityContextHolder.getContext().authentication
+    if (auth == null || !auth.isAuthenticated || auth.principal == null) {
+      throw InsufficientAuthenticationException(
+        "User is not authenticated or principal is null"
+      )
+    }
+    return auth.name
+  }
+}
