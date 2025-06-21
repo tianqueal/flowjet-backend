8980218f71c2a47a63829da38ecf3573ed0de7b6
Christian Alvarado <tianqueal@gmail.com>
2025-06-21 21:20:23 +0200

    feat: add ADMIN role to DefaultRoles

--- src/main/kotlin/com/tianqueal/flowjet/backend/utils/constants/DefaultRoles.kt
+++ src/main/kotlin/com/tianqueal/flowjet/backend/utils/constants/DefaultRoles.kt
@@ -4,4 +4,5 @@ import com.tianqueal.flowjet.backend.utils.enums.RoleName
 
 object DefaultRoles {
   val USER: Set<RoleName> = setOf(RoleName.ROLE_USER)
+  val ADMIN: Set<RoleName> = setOf(RoleName.ROLE_ADMIN)
 }
