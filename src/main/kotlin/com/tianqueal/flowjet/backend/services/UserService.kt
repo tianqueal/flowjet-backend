c30fbf233c16e24a8700ebb5c6c8c7d87dde11f6
Christian Alvarado <tianqueal@gmail.com>
2025-06-21 21:42:28 +0200

    feat: add UserService interface for user management operations

---
+++ src/main/kotlin/com/tianqueal/flowjet/backend/services/UserService.kt
@@ -0,0 +1,27 @@
+package com.tianqueal.flowjet.backend.services
+
+import com.tianqueal.flowjet.backend.domain.dto.v1.auth.UserRegisterRequest
+import com.tianqueal.flowjet.backend.domain.dto.v1.user.CreateAdminUserRequest
+import com.tianqueal.flowjet.backend.domain.dto.v1.user.CreateUserRequest
+import com.tianqueal.flowjet.backend.domain.dto.v1.user.UpdateUserByAdminRequest
+import com.tianqueal.flowjet.backend.domain.dto.v1.user.UpdateUserSelfRequest
+import com.tianqueal.flowjet.backend.domain.dto.v1.user.UserResponse
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+
+interface UserService {
+  fun findAll(username: String?, email: String?, name: String?, pageable: Pageable): Page<UserResponse>
+  fun findById(id: Long): UserResponse
+  fun findByUsername(username: String): UserResponse
+  fun findByUsernameOrEmail(usernameOrEmail: String): UserResponse
+
+  fun createAdminUser(createAdminUserRequest: CreateAdminUserRequest): UserResponse
+  fun createUserByAdmin(createUserRequest: CreateUserRequest): UserResponse
+  fun registerUser(userRegisterRequest: UserRegisterRequest): UserResponse
+
+  fun updateUserByAdmin(id: Long, updateUserByAdminRequest: UpdateUserByAdminRequest): UserResponse
+  fun updateCurrentUser(username: String, updateUserSelfRequest: UpdateUserSelfRequest): UserResponse
+
+  fun markUserAsVerifiedByUsername(username: String)
+  fun markUserAsNotVerifiedByUsername(username: String)
+}
