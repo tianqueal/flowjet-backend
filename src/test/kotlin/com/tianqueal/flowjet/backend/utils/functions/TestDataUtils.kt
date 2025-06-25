51e2bae66401d3ded5dd2aefdb225ca6774b7a54
Christian Alvarado <tianqueal@gmail.com>
2025-06-25 13:33:37 +0200

    feat: add utility function to create test project requests in TestDataUtils

--- src/test/kotlin/com/tianqueal/flowjet/backend/utils/functions/TestDataUtils.kt
+++ src/test/kotlin/com/tianqueal/flowjet/backend/utils/functions/TestDataUtils.kt
@@ -3,10 +3,11 @@ package com.tianqueal.flowjet.backend.utils.functions
 import com.tianqueal.flowjet.backend.domain.dto.v1.auth.UserRegisterRequest
 import com.tianqueal.flowjet.backend.domain.dto.v1.user.CreateUserRequest
 import com.tianqueal.flowjet.backend.domain.dto.v1.auth.LoginRequest
+import com.tianqueal.flowjet.backend.domain.dto.v1.project.CreateProjectRequest
 
 object TestDataUtils {
   const val DEFAULT_USERNAME = "test.user"
-  const val DEFAULT_EMAIL = "test.user@example.com"
+  const val DEFAULT_EMAIL = "$DEFAULT_USERNAME@example.com"
   const val DEFAULT_NAME = "Test User"
   const val DEFAULT_PASSWORD = "SecureP@ssw0rd"
 
@@ -15,21 +16,43 @@ object TestDataUtils {
     email: String = DEFAULT_EMAIL,
     name: String = DEFAULT_NAME,
     password: String = DEFAULT_PASSWORD,
-    avatarUrl: String? = null
+    avatarUrl: String? = null,
   ) = CreateUserRequest(username, email, name, password, avatarUrl)
 
   fun createTestUserRegisterRequest(
     username: String = DEFAULT_USERNAME,
     email: String = DEFAULT_EMAIL,
     name: String = DEFAULT_NAME,
-    password: String = DEFAULT_PASSWORD
+    password: String = DEFAULT_PASSWORD,
   ) = UserRegisterRequest(username, email, name, password)
 
   fun createTestLoginRequest(
     usernameOrEmail: String = DEFAULT_USERNAME,
     password: String = DEFAULT_PASSWORD,
-    rememberMe: Boolean = false
+    rememberMe: Boolean = false,
   ) = LoginRequest(
     usernameOrEmail, password, rememberMe
+  )
+
+  fun createTestProjectRequest(
+    name: String = "Test Project",
+    description: String = "Test project description",
+    projectStatusId: Int = 1,
+  ): CreateProjectRequest = CreateProjectRequest(
+    name = name,
+    description = description,
+    projectStatusId = projectStatusId
+  )
+
+  fun createTestUserRequest(
+    username: String = DEFAULT_USERNAME,
+    email: String = DEFAULT_EMAIL,
+    name: String = DEFAULT_NAME,
+    password: String = DEFAULT_PASSWORD,
+  ): CreateUserRequest = CreateUserRequest(
+    username = username,
+    email = email,
+    name = name,
+    password = password
   )
 }
