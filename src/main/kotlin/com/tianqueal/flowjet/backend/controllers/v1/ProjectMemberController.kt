e553bc9102329a37437d79e8d8a7f1617e9b2d80
Christian Alvarado <tianqueal@gmail.com>
2025-06-25 13:32:07 +0200

    feat: add ProjectMemberController for managing project members

---
+++ src/main/kotlin/com/tianqueal/flowjet/backend/controllers/v1/ProjectMemberController.kt
@@ -0,0 +1,36 @@
+package com.tianqueal.flowjet.backend.controllers.v1
+
+import com.tianqueal.flowjet.backend.domain.dto.v1.project.ProjectMemberResponse
+import com.tianqueal.flowjet.backend.services.ProjectMemberService
+import com.tianqueal.flowjet.backend.utils.constants.ApiPaths
+import com.tianqueal.flowjet.backend.utils.constants.SecurityConstants
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.data.web.PagedModel
+import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("${ApiPaths.V1}${ApiPaths.PROJECTS}/{id}")
+@PreAuthorize("isAuthenticated()")
+@SecurityRequirement(name = SecurityConstants.SECURITY_SCHEME_BEARER)
+@Tag(
+  name = "Project Member Management",
+  description = "Endpoints for managing project members"
+)
+class ProjectMemberController(
+  private val projectMemberService: ProjectMemberService,
+) {
+  @GetMapping(ApiPaths.MEMBERS)
+  fun getProjectMembers(
+    @Parameter(description = "ID of the project to retrieve members for")
+    @PathVariable id: Long,
+  ): ResponseEntity<PagedModel<ProjectMemberResponse>> {
+    return ResponseEntity.ok(null)
+  }
+}
