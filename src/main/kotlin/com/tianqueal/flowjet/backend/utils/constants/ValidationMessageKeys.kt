a694fc37e088b27929353972ff345d32f0e0880a
Christian Alvarado <tianqueal@gmail.com>
2025-06-25 12:57:37 +0200

    feat: add validation messages for project name and description

--- src/main/kotlin/com/tianqueal/flowjet/backend/utils/constants/ValidationMessageKeys.kt
+++ src/main/kotlin/com/tianqueal/flowjet/backend/utils/constants/ValidationMessageKeys.kt
@@ -21,4 +21,9 @@ object ValidationMessageKeys {
   // Validation - auth
   const val VALIDATION_AUTH_USERNAME_OR_EMAIL_NOT_BLANK = "{validation.auth.usernameOrEmail.notBlank}"
   const val VALIDATION_AUTH_PASSWORD_NOT_NULL = "{validation.auth.password.notNull}"
+  const val VALIDATION_AUTH_TOKEN_NOT_BLANK = "{validation.auth.token.notBlank}"
+
+  const val VALIDATION_PROJECT_NAME_NOT_BLANK = "{validation.project.name.notBlank}"
+  const val VALIDATION_PROJECT_NAME_SIZE = "{validation.project.name.size}"
+  const val VALIDATION_PROJECT_DESCRIPTION_SIZE = "{validation.project.description.size}"
 }
