8c7ce45561b9be9b7892d992e5d51eab59613882
Christian Alvarado <tianqueal@gmail.com>
2025-06-25 13:26:47 +0200

    refactor: rename RoleMapper and update RoleName to RoleEnum for consistency

--- src/main/kotlin/com/tianqueal/flowjet/backend/mappers/v1/RoleMapper.kt
+++ src/main/kotlin/com/tianqueal/flowjet/backend/mappers/v1/RoleMapper.kt
@@ -1,12 +1,12 @@
-package com.tianqueal.flowjet.backend.mappers
+package com.tianqueal.flowjet.backend.mappers.v1
 
 import com.tianqueal.flowjet.backend.domain.dto.v1.role.RoleResponse
-import com.tianqueal.flowjet.backend.utils.enums.RoleName
+import com.tianqueal.flowjet.backend.utils.enums.RoleEnum
 import org.springframework.stereotype.Component
 
 @Component
 class RoleMapper {
-  fun toDto(code: RoleName, name: String): RoleResponse = RoleResponse(
+  fun toDto(code: RoleEnum, name: String): RoleResponse = RoleResponse(
     code = code,
     name = name
   )
