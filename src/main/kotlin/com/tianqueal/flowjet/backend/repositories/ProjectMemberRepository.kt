93639bbf358a0e72a706a3f329fbfc43e80d74dc
Christian Alvarado <tianqueal@gmail.com>
2025-06-25 13:21:54 +0200

    feat: add ProjectMemberRepository for managing ProjectMemberEntity persistence

---
+++ src/main/kotlin/com/tianqueal/flowjet/backend/repositories/ProjectMemberRepository.kt
@@ -0,0 +1,18 @@
+package com.tianqueal.flowjet.backend.repositories
+
+import com.tianqueal.flowjet.backend.domain.entities.ProjectMemberEntity
+import com.tianqueal.flowjet.backend.domain.entities.keys.ProjectMemberId
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.data.jpa.repository.Query
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ProjectMemberRepository : JpaRepository<ProjectMemberEntity, ProjectMemberId>, JpaSpecificationExecutor<ProjectMemberEntity> {
+  @Query("""
+    SELECT CASE WHEN COUNT(pm) > 0 THEN TRUE ELSE FALSE END
+    FROM ProjectMemberEntity pm
+    WHERE pm.id.projectId = :projectId AND pm.id.userId = :userId
+  """)
+  fun isMember(projectId: Long, userId: Long): Boolean
+}
