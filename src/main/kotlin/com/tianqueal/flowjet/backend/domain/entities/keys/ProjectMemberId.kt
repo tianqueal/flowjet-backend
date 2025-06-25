0391df02c23d14df71a9aa65308bf1f4cd43d724
Christian Alvarado <tianqueal@gmail.com>
2025-06-25 13:18:34 +0200

    feat: add ProjectMemberId class for composite key representation in project members

---
+++ src/main/kotlin/com/tianqueal/flowjet/backend/domain/entities/keys/ProjectMemberId.kt
@@ -0,0 +1,26 @@
+package com.tianqueal.flowjet.backend.domain.entities.keys
+
+import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
+import java.io.Serializable
+import java.util.Objects
+
+@Embeddable
+class ProjectMemberId(
+  @Column(name = "project_id")
+  var projectId: Long,
+
+  @Column(name = "user_id")
+  var userId: Long
+) : Serializable {
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (javaClass != other?.javaClass) return false
+
+    other as ProjectMemberId
+
+    return projectId == other.projectId && userId == other.userId
+  }
+
+  override fun hashCode(): Int = Objects.hash(projectId, userId)
+}
