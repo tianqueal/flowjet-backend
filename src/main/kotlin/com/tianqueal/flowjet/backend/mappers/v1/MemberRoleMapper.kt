18376f023ce3da134c6d84179f8a90538682fd55
Christian Alvarado <tianqueal@gmail.com>
2025-06-25 13:19:34 +0200

    feat: add MemberRoleMapper for mapping MemberRoleEntity to MemberRoleResponse

---
+++ src/main/kotlin/com/tianqueal/flowjet/backend/mappers/v1/MemberRoleMapper.kt
@@ -0,0 +1,12 @@
+package com.tianqueal.flowjet.backend.mappers.v1
+
+import com.tianqueal.flowjet.backend.domain.dto.v1.project.MemberRoleResponse
+import com.tianqueal.flowjet.backend.domain.entities.MemberRoleEntity
+
+class MemberRoleMapper {
+  fun toDto(entity: MemberRoleEntity): MemberRoleResponse = MemberRoleResponse(
+    id = entity.id ?: -1,
+    code = entity.code,
+    name = entity.name,
+  )
+}
