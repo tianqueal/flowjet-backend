03b98cc23939c3bf51dbadb4dcd0c7acac48225b
Christian Alvarado <tianqueal@gmail.com>
2025-06-25 13:22:42 +0200

    feat: add AuthenticatedUserService for retrieving authenticated user details

---
+++ src/main/kotlin/com/tianqueal/flowjet/backend/services/AuthenticatedUserService.kt
@@ -0,0 +1,30 @@
+package com.tianqueal.flowjet.backend.services
+
+import com.tianqueal.flowjet.backend.domain.entities.UserEntity
+import com.tianqueal.flowjet.backend.exceptions.business.UserNotFoundException
+import com.tianqueal.flowjet.backend.repositories.UserRepository
+import com.tianqueal.flowjet.backend.utils.constants.BeanNames
+import com.tianqueal.flowjet.backend.utils.functions.AuthFunctions
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service(BeanNames.AUTHENTICATED_USER_SERVICE)
+@Transactional(readOnly = true)
+class AuthenticatedUserService(
+  private val userRepository: UserRepository,
+) {
+  fun getAuthenticatedUserEntity(): UserEntity {
+    val username = AuthFunctions.getAuthenticatedUsername()
+
+    return userRepository.findByUsername(username)
+      ?: throw UserNotFoundException(username)
+  }
+
+  fun getAuthenticatedUserId(): Long {
+    val user = getAuthenticatedUserEntity()
+
+    return requireNotNull(user.id) {
+      "Authenticated user '${user.username}' has a null ID, which is not permitted."
+    }
+  }
+}
