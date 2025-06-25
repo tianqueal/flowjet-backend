ab777cd5b5a97e1992056de736de6afa3bf8b39e
Christian Alvarado <tianqueal@gmail.com>
2025-06-25 13:06:03 +0200

    feat: refactor UserSpecification to use metamodel for field access

--- src/main/kotlin/com/tianqueal/flowjet/backend/specifications/UserSpecification.kt
+++ src/main/kotlin/com/tianqueal/flowjet/backend/specifications/UserSpecification.kt
@@ -1,6 +1,7 @@
 package com.tianqueal.flowjet.backend.specifications
 
 import com.tianqueal.flowjet.backend.domain.entities.UserEntity
+import com.tianqueal.flowjet.backend.domain.entities.UserEntity_
 import jakarta.persistence.criteria.Predicate
 import org.springframework.data.jpa.domain.Specification
 
@@ -8,26 +9,24 @@ object UserSpecification {
   fun filterBy(
     username: String?,
     email: String?,
-    name: String?
-  ): Specification<UserEntity> {
-    return Specification { root, _, cb ->
-      val predicates = mutableListOf<Predicate>()
+    name: String?,
+  ): Specification<UserEntity> = Specification { root, _, cb ->
+    val predicates = mutableListOf<Predicate>()
 
-      if (!username.isNullOrBlank()) {
-        predicates += cb.like(
-          cb.lower(root.get("username")), "%${username.lowercase()}%"
-        )
-      }
-
-      if (!email.isNullOrBlank()) {
-        predicates += cb.like(cb.lower(root.get("email")), "%${email.lowercase()}%")
-      }
-
-      if (!name.isNullOrBlank()) {
-        predicates += cb.like(cb.lower(root.get("name")), "%${name.lowercase()}%")
-      }
-
-      cb.and(*predicates.toTypedArray())
+    if (!username.isNullOrBlank()) {
+      predicates += cb.like(
+        cb.lower(root.get(UserEntity_.username)), "%${username.lowercase()}%"
+      )
     }
+
+    if (!email.isNullOrBlank()) {
+      predicates += cb.like(cb.lower(root.get(UserEntity_.email)), "%${email.lowercase()}%")
+    }
+
+    if (!name.isNullOrBlank()) {
+      predicates += cb.like(cb.lower(root.get(UserEntity_.name)), "%${name.lowercase()}%")
+    }
+
+    cb.and(*predicates.toTypedArray())
   }
 }
