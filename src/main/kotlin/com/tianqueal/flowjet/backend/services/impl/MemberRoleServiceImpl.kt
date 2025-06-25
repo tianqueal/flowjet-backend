0ce39d1fdc0c11bb81a3ede610f2dc2883cfa3ed
Christian Alvarado <tianqueal@gmail.com>
2025-06-25 13:28:36 +0200

    feat: add MemberRoleServiceImpl for managing member roles

---
+++ src/main/kotlin/com/tianqueal/flowjet/backend/services/impl/MemberRoleServiceImpl.kt
@@ -0,0 +1,15 @@
+package com.tianqueal.flowjet.backend.services.impl
+
+import com.tianqueal.flowjet.backend.domain.dto.v1.project.MemberRoleResponse
+import com.tianqueal.flowjet.backend.mappers.v1.MemberRoleMapper
+import com.tianqueal.flowjet.backend.repositories.MemberRoleRepository
+import com.tianqueal.flowjet.backend.services.MemberRoleService
+
+class MemberRoleServiceImpl(
+  private val memberRoleRepository: MemberRoleRepository,
+  private val memberRoleMapper: MemberRoleMapper,
+) : MemberRoleService {
+  override fun findAll(): List<MemberRoleResponse> =
+    memberRoleRepository.findAll()
+      .map(memberRoleMapper::toDto)
+}
