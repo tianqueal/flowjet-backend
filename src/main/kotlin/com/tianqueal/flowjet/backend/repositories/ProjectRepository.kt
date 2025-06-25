32755d1628f5296f072484f523bf00e7215e4070
Christian Alvarado <tianqueal@gmail.com>
2025-06-25 13:22:15 +0200

    feat: add ProjectRepository for managing ProjectEntity persistence

---
+++ src/main/kotlin/com/tianqueal/flowjet/backend/repositories/ProjectRepository.kt
@@ -0,0 +1,9 @@
+package com.tianqueal.flowjet.backend.repositories
+
+import com.tianqueal.flowjet.backend.domain.entities.ProjectEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ProjectRepository : JpaRepository<ProjectEntity, Long>, JpaSpecificationExecutor<ProjectEntity>
