54b45e70cf1a4db3111b24237ef82e93073024fe
Christian Alvarado <tianqueal@gmail.com>
2025-06-25 13:28:21 +0200

    feat: implement ProjectStatusService with ProjectStatusImpl

---
+++ src/main/kotlin/com/tianqueal/flowjet/backend/services/impl/ProjectStatusImpl.kt
@@ -0,0 +1,15 @@
+package com.tianqueal.flowjet.backend.services.impl
+
+import com.tianqueal.flowjet.backend.domain.dto.v1.project.ProjectStatusResponse
+import com.tianqueal.flowjet.backend.mappers.v1.ProjectStatusMapper
+import com.tianqueal.flowjet.backend.repositories.ProjectStatusRepository
+import com.tianqueal.flowjet.backend.services.ProjectStatusService
+
+class ProjectStatusImpl(
+  private val projectStatusRepository: ProjectStatusRepository,
+  private val projectStatusMapper: ProjectStatusMapper,
+) : ProjectStatusService {
+  override fun finAll(): List<ProjectStatusResponse> =
+    projectStatusRepository.findAll()
+      .map(projectStatusMapper::toDto)
+}
