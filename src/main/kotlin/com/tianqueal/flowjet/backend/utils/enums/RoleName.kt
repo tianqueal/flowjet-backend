b8eaabf66a281e9fc8f710a7eef2c647bf6376f8
Christian Alvarado <tianqueal@gmail.com>
2025-06-25 13:11:54 +0200

    rm: RoleName enum

--- src/main/kotlin/com/tianqueal/flowjet/backend/utils/enums/RoleName.kt
+++
@@ -1,9 +0,0 @@
-package com.tianqueal.flowjet.backend.utils.enums
-
-enum class RoleName(val displayName: String) {
-  ROLE_ADMIN("Administrator"),
-  ROLE_USER("User");
-
-  fun getCode(): String = name
-  fun shortName(): String = name.removePrefix("ROLE_")
-}
