fc05c0152bddca10d7a204d0c7e5a996c910d9a2
Christian Alvarado <tianqueal@gmail.com>
2025-06-25 13:30:45 +0200

    feat: add UserProfileController for managing user profiles

---
+++ src/main/kotlin/com/tianqueal/flowjet/backend/controllers/v1/UserProfileController.kt
@@ -0,0 +1,19 @@
+package com.tianqueal.flowjet.backend.controllers.v1
+
+import com.tianqueal.flowjet.backend.utils.constants.ApiPaths
+import com.tianqueal.flowjet.backend.utils.constants.SecurityConstants
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("${ApiPaths.V1}${ApiPaths.PROFILES}")
+@PreAuthorize("isAuthenticated()")
+@SecurityRequirement(name = SecurityConstants.SECURITY_SCHEME_BEARER)
+@Tag(
+  name = "User Profile resource",
+  description = "Endpoints for managing user profiles",
+)
+class UserProfileController
