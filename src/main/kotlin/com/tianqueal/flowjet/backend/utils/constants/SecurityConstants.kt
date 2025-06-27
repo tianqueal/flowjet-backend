29bb64844e7c67ff7626116c5ab844147378594e
Christian Alvarado <tianqueal@gmail.com>
2025-06-27 19:37:29 +0200

    feat: add constants for project member invitation token expiration and purpose

--- src/main/kotlin/com/tianqueal/flowjet/backend/utils/constants/SecurityConstants.kt
+++ src/main/kotlin/com/tianqueal/flowjet/backend/utils/constants/SecurityConstants.kt
@@ -5,6 +5,7 @@ object SecurityConstants {
     const val EXTENDED_ACCESS_TOKEN_EXPIRATION_SECONDS: Long = 7 * 24 * 60 * 60L // 7 days
     const val DEFAULT_EMAIL_VERIFICATION_TOKEN_EXPIRATION_SECONDS: Long = 60 * 60L // 1 hour
     const val DEFAULT_PASSWORD_RESET_TOKEN_EXPIRATION_SECONDS: Long = 60 * 60L // 1 hour
+    const val DEFAULT_PROJECT_MEMBER_INVITATION_TOKEN_EXPIRATION_SECONDS: Long = 60 * 60L // 1 hour
 
     const val BEARER_FORMAT = "JWT"
     const val SECURITY_SCHEME_BEARER = "bearerAuth"
@@ -13,6 +14,7 @@ object SecurityConstants {
     const val TOKEN_PURPOSE_ACCESS = "access_token"
     const val TOKEN_PURPOSE_EMAIL_VERIFICATION = "email_verification"
     const val TOKEN_PURPOSE_PASSWORD_RESET = "password_reset"
+    const val TOKEN_PURPOSE_PROJECT_MEMBER_INVITATION = "project_member_invitation"
 
     const val AUTHORITIES_CLAIM_NAME = "authorities"
     const val AUTHORITIES_PREFIX = ""
