c229769115a81408e25dc24e6ba549b27d035253
Christian Alvarado <tianqueal@gmail.com>
2025-06-28 16:29:34 +0200

    feat: add Spring component annotation to MemberRoleMapper for dependency injection

--- src/main/kotlin/com/tianqueal/flowjet/backend/mappers/v1/MemberRoleMapper.kt
+++ src/main/kotlin/com/tianqueal/flowjet/backend/mappers/v1/MemberRoleMapper.kt
@@ -2,7 +2,9 @@ package com.tianqueal.flowjet.backend.mappers.v1
 
 import com.tianqueal.flowjet.backend.domain.dto.v1.project.MemberRoleResponse
 import com.tianqueal.flowjet.backend.domain.entities.MemberRoleEntity
+import org.springframework.stereotype.Component
 
+@Component
 class MemberRoleMapper {
     fun toDto(entity: MemberRoleEntity): MemberRoleResponse =
         MemberRoleResponse(
