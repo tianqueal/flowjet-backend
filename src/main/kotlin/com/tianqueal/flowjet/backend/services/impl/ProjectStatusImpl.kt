7d099b0795b2a97bb3f67a411ae3a9b57ed943f4
Christian Alvarado <tianqueal@gmail.com>
2025-06-28 16:03:10 +0200

    feat: add service and transactional annotations to ProjectStatusImpl

--- src/main/kotlin/com/tianqueal/flowjet/backend/services/impl/ProjectStatusImpl.kt
+++ src/main/kotlin/com/tianqueal/flowjet/backend/services/impl/ProjectStatusImpl.kt
@@ -4,7 +4,11 @@ import com.tianqueal.flowjet.backend.domain.dto.v1.project.ProjectStatusResponse
 import com.tianqueal.flowjet.backend.mappers.v1.ProjectStatusMapper
 import com.tianqueal.flowjet.backend.repositories.ProjectStatusRepository
 import com.tianqueal.flowjet.backend.services.ProjectStatusService
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
+@Service
+@Transactional(readOnly = true)
 class ProjectStatusImpl(
     private val projectStatusRepository: ProjectStatusRepository,
     private val projectStatusMapper: ProjectStatusMapper,
