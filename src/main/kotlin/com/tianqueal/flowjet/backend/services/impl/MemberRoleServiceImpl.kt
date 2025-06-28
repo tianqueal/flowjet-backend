af2579806c75199429ff3a75798abb1bb214c090
Christian Alvarado <tianqueal@gmail.com>
2025-06-28 16:35:53 +0200

    feat: add Spring component and transaction management annotations to MemberRoleServiceImpl

--- src/main/kotlin/com/tianqueal/flowjet/backend/services/impl/MemberRoleServiceImpl.kt
+++ src/main/kotlin/com/tianqueal/flowjet/backend/services/impl/MemberRoleServiceImpl.kt
@@ -4,7 +4,11 @@ import com.tianqueal.flowjet.backend.domain.dto.v1.project.MemberRoleResponse
 import com.tianqueal.flowjet.backend.mappers.v1.MemberRoleMapper
 import com.tianqueal.flowjet.backend.repositories.MemberRoleRepository
 import com.tianqueal.flowjet.backend.services.MemberRoleService
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
+@Service
+@Transactional(readOnly = true)
 class MemberRoleServiceImpl(
     private val memberRoleRepository: MemberRoleRepository,
     private val memberRoleMapper: MemberRoleMapper,
