f13576579062a63e79d9e53e44a9b3201e1efe38
Christian Alvarado <tianqueal@gmail.com>
2025-07-08 12:08:38 +0200

    refactor: project permission checks for task assignee management

--- src/main/kotlin/com/tianqueal/flowjet/backend/services/ProjectPermissionService.kt
+++ src/main/kotlin/com/tianqueal/flowjet/backend/services/ProjectPermissionService.kt
@@ -5,14 +5,12 @@ import com.tianqueal.flowjet.backend.domain.entities.ProjectEntity
 import com.tianqueal.flowjet.backend.domain.entities.TaskEntity
 import com.tianqueal.flowjet.backend.domain.entities.keys.ProjectMemberId
 import com.tianqueal.flowjet.backend.exceptions.business.MemberRoleNotFoundException
-import com.tianqueal.flowjet.backend.exceptions.business.ProjectNotFoundException
 import com.tianqueal.flowjet.backend.repositories.MemberRoleRepository
 import com.tianqueal.flowjet.backend.repositories.ProjectMemberRepository
 import com.tianqueal.flowjet.backend.repositories.ProjectRepository
 import com.tianqueal.flowjet.backend.utils.constants.BeanNames
 import com.tianqueal.flowjet.backend.utils.enums.MemberRoleEnum
 import jakarta.annotation.PostConstruct
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -31,23 +29,20 @@ class ProjectPermissionService(
             ?: throw MemberRoleNotFoundException(MemberRoleEnum.PROJECT_VIEWER)
     }
 
-    fun isProjectOwner(
-        projectId: Long,
-        userId: Long,
-    ): Boolean {
-        val projectEntity = projectRepository.findByIdOrNull(projectId) ?: throw ProjectNotFoundException(projectId)
-        return isProjectOwner(projectEntity, userId)
-    }
+//    fun isProjectOwner(
+//        projectId: Long,
+//        userId: Long,
+//    ): Boolean = projectRepository.isOwner(projectId, userId)
 
     fun isProjectOwner(
         projectEntity: ProjectEntity,
         userId: Long,
     ): Boolean = projectEntity.owner.id == userId
 
-    fun isProjectMember(
-        projectId: Long,
-        userId: Long,
-    ): Boolean = projectMemberRepository.isMember(projectId, userId)
+//    fun isProjectMember(
+//        projectId: Long,
+//        userId: Long,
+//    ): Boolean = projectMemberRepository.isMember(projectId, userId)
 
 //    fun isNotViewer(
 //        projectEntity: ProjectEntity,
@@ -64,9 +59,9 @@ class ProjectPermissionService(
     ): Boolean = taskEntity.owner.id == userId
 
     fun canAddMember(
-        id: Long,
+        projectEntity: ProjectEntity,
         userId: Long,
-    ): Boolean = isProjectOwner(id, userId)
+    ): Boolean = isProjectOwner(projectEntity, userId)
 
     fun canCreateTask(
         projectEntity: ProjectEntity,
@@ -77,36 +72,32 @@ class ProjectPermissionService(
         }
 
         val projectMember =
-            projectMemberRepository.findByIdOrNull(
-                ProjectMemberId(projectEntity.id ?: -1, userId),
-            )
-
-        if (projectMember == null) {
-            return false
-        }
+            projectMemberRepository.findWithMemberRoleById(
+                ProjectMemberId(projectEntity.safeId, userId),
+            ) ?: return false
 
         return projectMember.memberRole.id != viewerRole.id
     }
 
     fun canUpdateMemberRole(
-        id: Long,
+        projectEntity: ProjectEntity,
         userId: Long,
-    ): Boolean = isProjectOwner(id, userId)
+    ): Boolean = isProjectOwner(projectEntity, userId)
 
     fun canRemoveMember(
-        id: Long,
+        projectEntity: ProjectEntity,
         userId: Long,
-    ): Boolean = isProjectOwner(id, userId)
+    ): Boolean = isProjectOwner(projectEntity, userId)
 
     fun canRead(
         id: Long,
         userId: Long,
-    ): Boolean = isProjectOwner(id, userId) || isProjectMember(id, userId)
+    ): Boolean = projectRepository.hasReadAccess(id, userId)
 
     fun canRead(
         projectEntity: ProjectEntity,
         userId: Long,
-    ): Boolean = isProjectOwner(projectEntity, userId) || isProjectMember(projectEntity.id ?: -1, userId)
+    ): Boolean = projectRepository.hasReadAccess(projectEntity.safeId, userId)
 
     fun canUpdateProject(
         projectEntity: ProjectEntity,
@@ -125,6 +116,18 @@ class ProjectPermissionService(
     ): Boolean = isProjectOwner(projectEntity, userId)
 
     fun canDeleteTask(
+        projectEntity: ProjectEntity,
+        taskEntity: TaskEntity,
+        userId: Long,
+    ): Boolean = isProjectOwner(projectEntity, userId) || isTaskOwner(taskEntity, userId)
+
+    fun canAddTaskAssignee(
+        projectEntity: ProjectEntity,
+        taskEntity: TaskEntity,
+        userId: Long,
+    ): Boolean = isProjectOwner(projectEntity, userId) || isTaskOwner(taskEntity, userId)
+
+    fun canRemoveTaskAssignee(
         projectEntity: ProjectEntity,
         taskEntity: TaskEntity,
         userId: Long,
