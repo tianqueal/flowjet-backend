4a5cd4d96a889bb05c8030c36e71a16e42355ffd
Christian Alvarado <tianqueal@gmail.com>
2025-07-16 11:16:05 +0200

    refactor: remove MAX_PAGE_SIZE constant from PaginationConstants

--- src/main/kotlin/com/tianqueal/flowjet/backend/utils/constants/PaginationConstants.kt
+++ src/main/kotlin/com/tianqueal/flowjet/backend/utils/constants/PaginationConstants.kt
@@ -3,5 +3,5 @@ package com.tianqueal.flowjet.backend.utils.constants
 object PaginationConstants {
     const val DEFAULT_PAGE_NUMBER = 0
     const val DEFAULT_PAGE_SIZE = 20
-    const val MAX_PAGE_SIZE = 100
+    // const val MAX_PAGE_SIZE = 100
 }
